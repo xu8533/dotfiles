e9891940b340022178963fc4a820ea2c34ab08d5
Xu Jian <838178533@qq.com>
2024-01-26 21:11:52 +0800

    添加fzf脚本

---
+++ .fzf.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/xujian/.fzf/bin* ]]; then
+	PATH="${PATH:+${PATH}:}/home/xujian/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+source "/home/xujian/.fzf/shell/completion.bash"
+
+# Key bindings
+# ------------
+source "/home/xujian/.fzf/shell/key-bindings.bash"
