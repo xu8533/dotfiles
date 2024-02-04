29bb1d330914761921a6d0a9555576bf1e1cd830
Xu Jian <838178533@qq.com>
2024-02-05 00:04:35 +0800

    修改start.sh内容，合并config_tmp和theme文件

--- .config/cava/start.sh
+++ .config/cava/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # 定义主题列表
 cava_theme_dir="$HOME/.config/cava/theme"
@@ -9,12 +9,15 @@ random_theme=$(ls "${cava_theme_dir}" | shuf -n 1)
 # 配置文件路径
 config_file="$HOME/.config/cava/config"
 
+# 配置模板
+config_tmp="$HOME/.config/cava/config_tmp"
+
 # 替换配置文件中的主题设置
 # sed -i "s/${config_file}/${random_theme}/" "${config_file}"
 
 notify-send -i $HOME/.config/cava/cava.png -t 2000 -u normal "cava主题修改" "主题为：${random_theme}"
 
-cat "$cava_theme_dir/$random_theme" >$config_file
+/usr/bin/cat $config_tmp $cava_theme_dir/$random_theme >$config_file
 
 # 启动 Cava
 cava
