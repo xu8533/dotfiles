2a5ea7f146f21d411f04524b426a6ab69e99ccb8
Xu Jian <838178533@qq.com>
2024-04-19 00:09:02 +0800

    disable this file

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -1,14 +1,15 @@
--- You can also add or configure plugins by creating files in this `plugins/` folder
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE-- You can also add or configure plugins by creating files in this `plugins/` folder
+
 -- Here are some examples:
 ---@type LazySpec
 
 return {
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function() require("lsp_signature").setup() end,
+  -- },
 
   -- customize alpha options
   -- {
@@ -36,15 +37,15 @@ return {
   -- { "max397574/better-escape.nvim", enabled = false},
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   config = function(plugin, opts)
+  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
+  --     local luasnip = require "luasnip"
+  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
+  --   end,
+  -- },
 
   -- {
   --   "windwp/nvim-autopairs",
