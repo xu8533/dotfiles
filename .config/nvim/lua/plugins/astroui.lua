54d8257c8afa0f6f997feff0adea693e050eed28
Xu Jian <838178533@qq.com>
2024-04-23 22:57:13 +0800

    change colorscheme to bamboo

--- .config/nvim/lua/plugins/astroui.lua
+++ .config/nvim/lua/plugins/astroui.lua
@@ -10,6 +10,7 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
+    colorscheme = "bamboo",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
