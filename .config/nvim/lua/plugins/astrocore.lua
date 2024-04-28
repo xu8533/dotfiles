727bdae13c91797ebcc2dcfd853ec7790d35e3cf
Xu Jian <838178533@qq.com>
2024-04-28 22:44:17 +0800

    modify <Leader>c mapping

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -123,10 +123,8 @@ return {
         ["<Leader>c"] = {
           function()
             local bufs = vim.fn.getbufinfo { buflisted = true }
-            require("astronvim.utils.buffer").close(0)
-            if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then
-              require("alpha").start(true)
-            end
+            require("astrocore.buffer").close(0)
+            if require("astrocore").is_available "alpha-nvim" and not bufs[2] then require("alpha").start() end
           end,
           desc = "Close buffer",
         },
