20de0fb8bf342623f0b49fc6a7c936f65e1789c3
Xu Jian <838178533@qq.com>
2024-09-08 16:49:22 +0800

    delete rooter code block, instead by astrocore_rooter.lua, and set wrap to true

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -63,29 +63,6 @@ return {
     git_worktrees = {
       { toplevel = vim.env.HOME, gitdir = vim.env.HOME .. "/.dotfiles" },
     },
-    -- Configure project root detection, check status with `:AstroRootInfo`
-    rooter = {
-      -- list of detectors in order of prevalence, elements can be:
-      --   "lsp" : lsp detection
-      --   string[] : a list of directory patterns to look for
-      --   fun(bufnr: integer): string|string[] : a function that takes a buffer number and outputs detected roots
-      detector = {
-        "lsp", -- highest priority is getting workspace from running language servers
-        { ".git", "_darcs", ".hg", ".bzr", ".svn" }, -- next check for a version controlled parent directory
-        { "lua", "MakeFile", "package.json" }, -- lastly check for known project root files
-      },
-      -- ignore things from root detection
-      ignore = {
-        servers = {}, -- list of language server names to ignore (Ex. { "efm" })
-        dirs = {}, -- list of directory patterns (Ex. { "~/.cargo/*" })
-      },
-      -- automatically update working directory (update manually with `:AstroRoot`)
-      autochdir = false,
-      -- scope of working directory to change ("global"|"tab"|"win")
-      scope = "global",
-      -- show notification on every working directory change
-      notify = false,
-    },
     -- Configuration table of session options for AstroNvim's session management powered by Resession
     sessions = {
       -- Configure auto saving
@@ -151,7 +128,7 @@ return {
         numberwidth = 3, -- 设置行号宽度
         spell = false, -- sets vim.opt.spell
         signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
         sidescrolloff = 8, -- 保留光标周围8列
         -- tabstop = 4,
         -- softtabstop = 4,
