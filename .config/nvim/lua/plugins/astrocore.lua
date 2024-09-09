eca959dd1f938d5fd315b89c4df547172eb755b3
Xu Jian <838178533@qq.com>
2024-09-09 20:16:41 +0800

    enable autopairs

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -35,7 +35,7 @@ return {
     },
     features = {
       large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      -- autopairs = true, -- enable autopairs at start
+      autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
       diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
       highlighturl = true, -- highlight URLs at start
