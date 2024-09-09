2d47d0543a52c170328ecb02cb5299077368ba06
Xu Jian <838178533@qq.com>
2024-09-09 20:18:28 +0800

    add bars-and-lines part

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -5,6 +5,13 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- bars-and-lines
+  -- { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.feline-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.statuscol-nvim" },
   -- color
   { import = "astrocommunity.color.ccc-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
