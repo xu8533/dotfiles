d33f0ab65a949ab4a3579cebc3d0f4706222fb59
Xu Jian <838178533@qq.com>
2024-09-12 09:47:27 +0800

    <A-s> instead of s command, conflict with flash.nvim

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -26,7 +26,7 @@ return {
     opts = {
       mappings = {
         n = {
-          ["s"] = { function() require("hop").hint_words() end, desc = "Hop hint words" },
+          ["<A-s>"] = { function() require("hop").hint_words() end, desc = "Hop hint words" },
           -- ["<S-s>"] = { function() require("hop").hint_lines() end, desc = "Hop hint lines" },
         },
         v = {
