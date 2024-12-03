dc37e4da2fd7ab347b617ac28509bddaf71f7a34
Xu Jian <838178533@qq.com>
2024-12-03 23:37:59 +0800

    根据文件类型调整配色方案

---
+++ .config/nvim/lua/plugins/styler.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/styler.nvim",
+  opts = {
+    themes = {
+      -- markdown = { colorscheme = "tokyonight-moon"},
+      -- markdown = { colorscheme = "kanagawa-paper" },
+      markdown = { colorscheme = "catppuccin-macchiato" },
+      -- markdown = { colorscheme = "rose-pine-main" },
+      -- help = { colorscheme = "catppuccin-mocha", background = "dark" },
+    },
+  },
+}
