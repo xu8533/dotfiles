528315e31807953da92a8249fd38f532c035e60e
Xu Jian <838178533@qq.com>
2024-12-14 23:57:30 +0800

    删除部分注释

--- .fzf.bash
+++ .fzf.bash
@@ -5,15 +5,6 @@
 # 	PATH="${PATH:+${PATH}:}/home/xujian/.fzf/bin"
 # fi
 
-# Auto-completion
-# ---------------
-# source "/home/xujian/.fzf/shell/completion.bash"
-
-# Key bindings
-# ------------
-# source "/home/xujian/.fzf/shell/key-bindings.bash"
-#
-#
 #
 source "/usr/share/fzf/completion.bash"
 source "/usr/share/fzf/key-bindings.bash"
