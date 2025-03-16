cf5efcbeeb2ee877c62f26640c3b22072ed28832
Xu Jian <838178533@qq.com>
2025-03-16 12:29:13 +0800

    窗口左右大小调整键为<Shift-Left>,<Shift-Right>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
+local del = vim.keymap.del
 
 map({ "n", "x" }, ";", ":", { desc = "quick enter command mode" })
+
+-- 调整窗口左右大小
+del("n", "<C-Left>")
+del("n", "<C-Right>")
+map("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
