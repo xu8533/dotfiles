6bfcf748377c477c053729161558f1f8799c934a
Xu Jian <838178533@qq.com>
2025-04-16 21:25:20 +0800

    delete duplicate line

--- .config/hypr/scripts/randwall.sh
+++ .config/hypr/scripts/randwall.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-swww img $(find $wallpaper_path -type f | shuf -n 1) --transition-type any --transition-fps 60/bin/bash
 swww img $(find $wallpaper_path -type f | shuf -n 1) --transition-type any --transition-fps 60
