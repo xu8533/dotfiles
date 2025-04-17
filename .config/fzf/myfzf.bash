a3a3da286cc97192566eec089d055f210dbfa03a
Xu Jian <838178533@qq.com>
2025-04-17 23:37:59 +0800

    add --ghost=请输入

--- .config/fzf/myfzf.bash
+++ .config/fzf/myfzf.bash
@@ -71,6 +71,7 @@ export FZF_DEFAULT_OPTS="
     --header-lines-border bottom
     --list-border bottom
     --style=full
+    --ghost=请输入
 "
 
 # fzf和tmux结合使用
@@ -89,70 +90,70 @@ export FZF_DEFAULT_OPTS="
 #############重写部分默认函数#######################################
 # 使用fd替换find
 _fzf_compgen_path() {
-	fd --hidden --follow --exclude ".git" . "$1"
+    fd --hidden --follow --exclude ".git" . "$1"
 }
 _fzf_compgen_dir() {
-	fd --type d --hidden --follow --exclude ".git" . "$1"
+    fd --type d --hidden --follow --exclude ".git" . "$1"
 }
 # Advanced customization of fzf options via _fzf_comprun function
 # - The first argument to the function is the name of the command.
 # - You should maje sure to pass the rest of the arguments to fzf.
 _fzf_comprun() {
-	local command=$1
-	shift
+    local command=$1
+    shift
 
-	case "$command" in
-	cd) fzf --preview 'tree -C {} | head -200' "$@" ;;
-	export | unset) fzf --preview "eval 'echo \$'{}" "$@" ;;
-	ssh) fzf --preview 'dig {}' "$@" ;;
-	*) fzf --preview 'bat -n --color=always {}' "$@" ;;
-	esac
+    case "$command" in
+    cd) fzf --preview 'tree -C {} | head -200' "$@" ;;
+    export | unset) fzf --preview "eval 'echo \$'{}" "$@" ;;
+    ssh) fzf --preview 'dig {}' "$@" ;;
+    *) fzf --preview 'bat -n --color=always {}' "$@" ;;
+    esac
 }
 
 # To use for Ctrl-t for a floating menu from terminal
 __fzfmenu__() {
-	local cmd="fd -tf --max-depth=1"
-	eval "$cmd" | ~/.local/bin/fzfmenu
-	# eval "$cmd"
+    local cmd="fd -tf --max-depth=1"
+    eval "$cmd" | ~/.local/bin/fzfmenu
+    # eval "$cmd"
 }
 
 __fzf-menu__() {
-	local selected="$(__fzfmenu__)"
-	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
-	READLINE_POINT=$((READLINE_POINT + ${#selected}))
+    local selected="$(__fzfmenu__)"
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
+    READLINE_POINT=$((READLINE_POINT + ${#selected}))
 }
 bind -x '"\C-t":"__fzf-menu__"'
 
 #############社区提供的一些常用函数################################
 # fkill - 杀死进程- 只显示本账号可以kill的进程
 fkill() {
-	local pid
-	if [ "$UID" != "0" ]; then
-		pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
-	else
-		pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-	fi
+    local pid
+    if [ "$UID" != "0" ]; then
+        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+    else
+        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    fi
 
-	if [ "x$pid" != "x" ]; then
-		echo $pid | xargs kill -${1:-9}
-	fi
+    if [ "x$pid" != "x" ]; then
+        echo $pid | xargs kill -${1:-9}
+    fi
 }
 
 # fman - 查看man手册
 man-find() {
-	f=$(fd . $MANPATH/man${1:-1} -t f -x echo {/.} | fzf) && man $f
+    f=$(fd . $MANPATH/man${1:-1} -t f -x echo {/.} | fzf) && man $f
 }
 
 fman() {
-	man -k . | fzf -q "$1" --prompt='man> ' --preview $'
+    man -k . | fzf -q "$1" --prompt='man> ' --preview $'
 		echo {} | tr -d \'()\' | 
 		awk \'{printf "%s ", $2} {print $1}\' | 
 		xargs -r man | 
 		col -bx | 
 		bat -l man -p --color always
 		' | tr -d '()' |
-		awk '{printf "%s ", $2} {print $1}' |
-		xargs -r man
+        awk '{printf "%s ", $2} {print $1}' |
+        xargs -r man
 }
 # Get the colors in the opened man page itself
 # export MANPAGER="sh -c 'col -bx | bat -l man -p --paging always'"
@@ -164,69 +165,69 @@ export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 #   - CTRL-O to open with `open` command,
 #   - CTRL-E or Enter key to open with the $EDITOR
 fo() {
-	IFS=$'\n' out=("$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)")
-	key=$(head -1 <<<"$out")
-	file=$(head -2 <<<"$out" | tail -1)
-	if [ -n "$file" ]; then
-		[ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-emacs} "$file"
-	fi
+    IFS=$'\n' out=("$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)")
+    key=$(head -1 <<<"$out")
+    file=$(head -2 <<<"$out" | tail -1)
+    if [ -n "$file" ]; then
+        [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-emacs} "$file"
+    fi
 }
 
 # fh - 重复历史命令
 runcmd() { perl -e 'ioctl STDOUT, 0x5412, $_ for split //, <>'; }
 fh() {
-	([ -n "$BASH" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | runcmd
+    ([ -n "$BASH" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | runcmd
 }
 
 # fhe - 重复历史命令(允许修改命令)
 writecmd() { perl -e 'ioctl STDOUT, 0x5412, $_ for split //, do{ chomp($_ = <>); $_ }'; }
 fhe() {
-	([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | writecmd
+    ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | writecmd
 }
 
 # git集成
 # fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
 fbr() {
-	local branches branch
-	branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-		branch=$(echo "$branches" |
-			fzf-tmux -d $((2 + $(wc -l <<<"$branches"))) +m) &&
-		git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+    local branches branch
+    branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+        branch=$(echo "$branches" |
+            fzf-tmux -d $((2 + $(wc -l <<<"$branches"))) +m) &&
+        git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 # fco - checkout git branch/tag
 fco() {
-	local tags branches target
-	branches=$(
-		git --no-pager branch --all \
-			--format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" |
-			sed '/^$/d'
-	) || return
-	tags=$(
-		git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}'
-	) || return
-	target=$(
-		(
-			echo "$branches"
-			echo "$tags"
-		) |
-			fzf --no-hscroll --no-multi -n 2 \
-				--ansi
-	) || return
-	git checkout $(awk '{print $2}' <<<"$target")
+    local tags branches target
+    branches=$(
+        git --no-pager branch --all \
+            --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" |
+            sed '/^$/d'
+    ) || return
+    tags=$(
+        git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}'
+    ) || return
+    target=$(
+        (
+            echo "$branches"
+            echo "$tags"
+        ) |
+            fzf --no-hscroll --no-multi -n 2 \
+                --ansi
+    ) || return
+    git checkout $(awk '{print $2}' <<<"$target")
 }
 # fcoc - checkout git commit
 fcoc() {
-	local commits commit
-	commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-		commit=$(echo "$commits" | fzf --tac +s +m -e) &&
-		git checkout $(echo "$commit" | sed "s/ .*//")
+    local commits commit
+    commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
+        commit=$(echo "$commits" | fzf --tac +s +m -e) &&
+        git checkout $(echo "$commit" | sed "s/ .*//")
 }
 # fshow - git commit browser
 fshow() {
-	git log --graph --color=always \
-		--format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-		fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-			--bind "ctrl-m:execute:
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+        fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+            --bind "ctrl-m:execute:
               (grep -o '[a-f0-9]\{7\}' | head -1 |
               xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
               {}
@@ -238,20 +239,20 @@ _gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
 _viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy'"
 # fcoc_preview - checkout git commit with previews
 fcoc_preview() {
-	local commit
-	commit=$(glNoGraph |
-		fzf --no-sort --reverse --tiebreak=index --no-multi \
-			--ansi --preview="$_viewGitLogLine") &&
-		git checkout $(echo "$commit" | sed "s/ .*//")
+    local commit
+    commit=$(glNoGraph |
+        fzf --no-sort --reverse --tiebreak=index --no-multi \
+            --ansi --preview="$_viewGitLogLine") &&
+        git checkout $(echo "$commit" | sed "s/ .*//")
 }
 # fshow_preview - git commit browser with previews
 fshow_preview() {
-	glNoGraph |
-		fzf --no-sort --reverse --tiebreak=index --no-multi \
-			--ansi --preview="$_viewGitLogLine" \
-			--header "enter to view, alt-y to copy hash" \
-			--bind "enter:execute:$_viewGitLogLine   | less -R" \
-			--bind "alt-y:execute:$_gitLogLineToHash | xclip"
+    glNoGraph |
+        fzf --no-sort --reverse --tiebreak=index --no-multi \
+            --ansi --preview="$_viewGitLogLine" \
+            --header "enter to view, alt-y to copy hash" \
+            --bind "enter:execute:$_viewGitLogLine   | less -R" \
+            --bind "alt-y:execute:$_gitLogLineToHash | xclip"
 }
 # fstash - easier way to deal with stashes
 # type fstash to get a list of your stashes
@@ -259,27 +260,27 @@ fshow_preview() {
 # ctrl-d shows a diff of the stash against your current HEAD
 # ctrl-b checks the stash out as a branch, for easier merging
 fstash() {
-	local out q k sha
-	while out=$(
-		git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-			fzf --ansi --no-sort --query="$q" --print-query \
-				--expect=ctrl-d,ctrl-b
-	); do
-		mapfile -t out <<<"$out"
-		q="${out[0]}"
-		k="${out[1]}"
-		sha="${out[-1]}"
-		sha="${sha%% *}"
-		[[ -z "$sha" ]] && continue
-		if [[ "$k" == 'ctrl-d' ]]; then
-			git diff $sha
-		elif [[ "$k" == 'ctrl-b' ]]; then
-			git stash branch "stash-$sha" $sha
-			break
-		else
-			git stash show -p $sha
-		fi
-	done
+    local out q k sha
+    while out=$(
+        git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
+            fzf --ansi --no-sort --query="$q" --print-query \
+                --expect=ctrl-d,ctrl-b
+    ); do
+        mapfile -t out <<<"$out"
+        q="${out[0]}"
+        k="${out[1]}"
+        sha="${out[-1]}"
+        sha="${sha%% *}"
+        [[ -z "$sha" ]] && continue
+        if [[ "$k" == 'ctrl-d' ]]; then
+            git diff $sha
+        elif [[ "$k" == 'ctrl-b' ]]; then
+            git stash branch "stash-$sha" $sha
+            break
+        else
+            git stash show -p $sha
+        fi
+    done
 }
 
 # gh-watch -- watch the current actions
@@ -294,23 +295,23 @@ fstash() {
 # ftags - search ctags with preview
 # only works if tags-file was generated with --excmd=number
 ftags() {
-	local line
-	[ -e tags ] &&
-		line=$(
-			awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
-				fzf \
-					--nth=1,2 \
-					--with-nth=2 \
-					--preview-window="50%" \
-					--preview="bat {3} --color=always | tail -n +\$(echo {4} | tr -d \";\\\"\")"
-		) && ${EDITOR:-vim} $(cut -f3 <<<"$line") -c "set nocst" \
-		-c "silent tag $(cut -f2 <<<"$line")"
+    local line
+    [ -e tags ] &&
+        line=$(
+            awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
+                fzf \
+                    --nth=1,2 \
+                    --with-nth=2 \
+                    --preview-window="50%" \
+                    --preview="bat {3} --color=always | tail -n +\$(echo {4} | tr -d \";\\\"\")"
+        ) && ${EDITOR:-vim} $(cut -f3 <<<"$line") -c "set nocst" \
+        -c "silent tag $(cut -f2 <<<"$line")"
 }
 
 # conda集成
 fzf-conda-activate() {
-	choice=(
-		$"
+    choice=(
+        $"
 			conda env list |
 				sed 's/\*/ /;1,2d' |
 				xargs -I {} bash -c '
@@ -331,64 +332,64 @@ fzf-conda-activate() {
                     sort
                 '
 		"
-	)
-	[[ -n "$choice" ]] && conda activate "$choice"
+    )
+    [[ -n "$choice" ]] && conda activate "$choice"
 }
 
 # fzf版本cd命令
 cd() {
-	# 修复直接使用cd命令不加参数时还是跳出目录选择菜单
-	if [[ "$#" != 0 ]]; then
-		builtin cd "$@" || return
-	elif [[ "$#" == 0 ]]; then
-		builtin cd
-		return
-	fi
-	while true; do
-		local lsd=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
-		local dir="$(printf '%s\n' "${lsd[@]}" |
-			fzf --reverse --preview '
+    # 修复直接使用cd命令不加参数时还是跳出目录选择菜单
+    if [[ "$#" != 0 ]]; then
+        builtin cd "$@" || return
+    elif [[ "$#" == 0 ]]; then
+        builtin cd
+        return
+    fi
+    while true; do
+        local lsd=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
+        local dir="$(printf '%s\n' "${lsd[@]}" |
+            fzf --reverse --preview '
                 __cd_nxt="$(echo {})";
                 __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")";
                 echo $__cd_path;
                 echo;
                 ls -p --color=always "${__cd_path}";
         ')"
-		[[ ${#dir} != 0 ]] || return 0
-		builtin cd "$dir" &>/dev/null || return
-	done
+        [[ ${#dir} != 0 ]] || return 0
+        builtin cd "$dir" &>/dev/null || return
+    done
 }
 
 # fzf和autojump集成
 fj() {
-	local preview_cmd="ls {2..}"
-	if command -v exa &>/dev/null; then
-		preview_cmd="exa -l {2}"
-	fi
+    local preview_cmd="ls {2..}"
+    if command -v exa &>/dev/null; then
+        preview_cmd="exa -l {2}"
+    fi
 
-	if [[ $# -eq 0 ]]; then
-		cd "$(autojump -s | sort -k1gr | awk -F : '$1 ~ /[0-9]/ && $2 ~ /^\s*\// {print $1 $2}' | fzf --height 40% --reverse --inline-info --preview "$preview_cmd" --preview-window down:50% | cut -d$'\t' -f2- | sed 's/^\s*//')"
-	else
-		cd $(autojump $@)
-	fi
+    if [[ $# -eq 0 ]]; then
+        cd "$(autojump -s | sort -k1gr | awk -F : '$1 ~ /[0-9]/ && $2 ~ /^\s*\// {print $1 $2}' | fzf --height 40% --reverse --inline-info --preview "$preview_cmd" --preview-window down:50% | cut -d$'\t' -f2- | sed 's/^\s*//')"
+    else
+        cd $(autojump $@)
+    fi
 }
 
 # mpd集成
 fmpc() {
-	local song_position
-	song_position=$(mpc -f "%position%) %artist% - %title%" playlist |
-		fzf-tmux --query="$1" --reverse --select-1 --exit-0 |
-		sed -n 's/^\([0-9]\+\)).*/\1/p') || return 1
-	[ -n "$song_position" ] && mpc -q play $song_position
+    local song_position
+    song_position=$(mpc -f "%position%) %artist% - %title%" playlist |
+        fzf-tmux --query="$1" --reverse --select-1 --exit-0 |
+        sed -n 's/^\([0-9]\+\)).*/\1/p') || return 1
+    [ -n "$song_position" ] && mpc -q play $song_position
 }
 
 # CTRL-X-1 - Invoke Readline functions by name
 __fzf_readline() {
-	builtin eval "
+    builtin eval "
         builtin bind ' \
             \"\C-x3\": $(
-		builtin bind -l | command fzf +s +m --toggle-sort=ctrl-r
-	) \
+        builtin bind -l | command fzf +s +m --toggle-sort=ctrl-r
+    ) \
         '
     "
 }
