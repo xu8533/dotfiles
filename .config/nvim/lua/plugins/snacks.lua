593b0f7eb0806ad75a7f7059e97556b265430c4f
Xu Jian <838178533@qq.com>
2025-07-31 21:46:49 +0800

    custom dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -12,23 +12,59 @@ return {
         preset = {
           pick = "fzf-lua",
           keys = {
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "f", desc = "查找文件", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "新建文件", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "搜索内容", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "最近文件", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "s", desc = "恢复会话", section = "session" },
             {
               icon = " ",
               key = "c",
-              desc = "Config",
+              desc = "配置neovim",
               action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
             },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            { icon = " ", key = "q", desc = "退出neovim", action = ":qa" },
           },
+          header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
         },
+        -- sections = {
+        --   { section = "header" },
+        --   { section = "keys", gap = 1, padding = 1 },
+        --   { section = "startup" },
+        -- },
         sections = {
           { section = "header" },
+          {
+            pane = 2,
+            section = "terminal",
+            -- cmd = "colorscript -e zwaves",
+            cmd = "sh ~/.local/share/asciiart/square",
+            height = 5,
+            padding = 1,
+          },
           { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "最近文件", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "工程", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
           { section = "startup" },
         },
       },
