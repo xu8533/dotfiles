7d148fa6286abcf655514bce2958fb611eadae57
Xu Jian <838178533@qq.com>
2025-09-22 22:45:23 +0800

    custom code block

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -2,6 +2,14 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
+      anti_conceal = {
+        disabled_modes = { "n" },
+        ignore = {
+          bullet = true, -- render bullet in insert mode
+          head_border = true,
+          head_background = true,
+        },
+      },
       bullet = {
         icons = {
           {
@@ -10,11 +18,6 @@ return {
           },
         },
       },
-      code = {
-        sign = false,
-        width = "block",
-        right_pad = 1,
-      },
       callout = {
         note = {
           rendered = "󰅾 Notary",
@@ -22,20 +25,67 @@ return {
         },
       },
       checkbox = {
-        enabled = true,
-        unchecked = { icon = "✘ " },
+        unchecked = {
+          icon = "󰄱",
+          highlight = "RenderMarkdownCodeFallback",
+          scope_highlight = "RenderMarkdownCodeFallback",
+        },
         checked = {
-          icon = " ",
-          scope_highlight = "@markup.strikethrough",
+          icon = "󰄵",
+          highlight = "RenderMarkdownUnchecked",
+          scope_highlight = "RenderMarkdownUnchecked",
         },
         custom = {
-          todo = { rendered = "󰄯 " },
+          question = {
+            raw = "[?]",
+            rendered = "",
+            highlight = "RenderMarkdownError",
+            scope_highlight = "RenderMarkdownError",
+          },
+          todo = {
+            raw = "[>]",
+            rendered = "󰦖",
+            highlight = "RenderMarkdownInfo",
+            scope_highlight = "RenderMarkdownInfo",
+          },
+          canceled = {
+            raw = "[-]",
+            rendered = "",
+            highlight = "RenderMarkdownCodeFallback",
+            scope_highlight = "@text.strike",
+          },
           important = {
+            raw = "[!]",
+            rendered = "",
+            highlight = "RenderMarkdownWarn",
+            scope_highlight = "RenderMarkdownWarn",
+          },
+          favorite = {
             raw = "[~]",
-            rendered = "󰓎 ",
-            highlight = "DiagnosticWarn",
+            rendered = "",
+            highlight = "RenderMarkdownMath",
+            scope_highlight = "RenderMarkdownMath",
           },
         },
+      },
+      code = {
+        -- 常规设置
+        width = "block",
+        min_width = 80,
+        -- 边框
+        border = "thin",
+        left_pad = 1,
+        right_pad = 1,
+        -- 语言信息
+        position = "right",
+        language_icon = true,
+        language_name = true,
+        -- avoid making headings ugly
+        highlight_inline = "RenderMarkdownCodeInfo",
+      },
+      completions = {
+        blink = { enabled = true },
+        lsp = { enabled = true },
       },
       dash = { width = 20 },
       heading = {
@@ -52,17 +102,24 @@ return {
       },
       latex = { bottom_pad = 1 },
       link = {
+        wiki = { icon = " ", highlight = "RenderMarkdownWikiLink", scope_highlight = "RenderMarkdownWikiLink" },
         image = "󰋵 ",
         email = " ",
         hyperlink = "󰌷 ",
         custom = {
-          python = {
-            pattern = "%.py$",
-            icon = "󰌠 ",
-          },
+          python = { pattern = "%.py$", icon = "󰌠 " },
+          github = { pattern = "github", icon = " " },
+          gitlab = { pattern = "gitlab", icon = "󰮠 " },
+          youtube = { pattern = "youtube", icon = " " },
+          cern = { pattern = "cern.ch", icon = " " },
         },
       },
+      pipe_table = {
+        alignment_indicator = "─",
+        border = { "╭", "┬", "╮", "├", "┼", "┤", "╰", "┴", "╯", "│", "─" },
+      },
       sign = { enabled = false },
+      win_options = { concealcursor = { rendered = "nvc" } },
     },
     ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
     config = function(_, opts)
