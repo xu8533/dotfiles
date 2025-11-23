064dcf938c6e62d4d760471ae3c2c54329bc5e2e
Xu Jian <838178533@qq.com>
2025-11-23 22:13:07 +0800

    lazyload this plugin, split config file to config/render-markdown, add depend plugins, stuff for img-clip.nvim plugin, add keybind for paste image

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,143 +1,55 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      anti_conceal = {
-        disabled_modes = { "n" },
-        ignore = {
-          bullet = true, -- render bullet in insert mode
-          head_border = true,
-          head_background = true,
-        },
-      },
-      bullet = {
-        icons = {
-          {
-            "",
-            "",
-          },
-        },
-      },
-      callout = {
-        note = {
-          rendered = "󰅾 Notary",
-          quote_icon = "█",
-        },
-      },
-      checkbox = {
-        unchecked = {
-          icon = "󰄱",
-          highlight = "RenderMarkdownCodeFallback",
-          scope_highlight = "RenderMarkdownCodeFallback",
-        },
-        checked = {
-          icon = "󰄵",
-          highlight = "RenderMarkdownUnchecked",
-          scope_highlight = "RenderMarkdownUnchecked",
-        },
-        custom = {
-          question = {
-            raw = "[?]",
-            rendered = "",
-            highlight = "RenderMarkdownError",
-            scope_highlight = "RenderMarkdownError",
-          },
-          todo = {
-            raw = "[>]",
-            rendered = "󰦖",
-            highlight = "RenderMarkdownInfo",
-            scope_highlight = "RenderMarkdownInfo",
-          },
-          canceled = {
-            raw = "[-]",
-            rendered = "",
-            highlight = "RenderMarkdownCodeFallback",
-            scope_highlight = "@text.strike",
-          },
-          important = {
-            raw = "[!]",
-            rendered = "",
-            highlight = "RenderMarkdownWarn",
-            scope_highlight = "RenderMarkdownWarn",
-          },
-          favorite = {
-            raw = "[~]",
-            rendered = "",
-            highlight = "RenderMarkdownMath",
-            scope_highlight = "RenderMarkdownMath",
-          },
-        },
-      },
-      code = {
-        -- 常规设置
-        width = "block",
-        min_width = 80,
-        -- 边框
-        border = "thin",
-        left_pad = 1,
-        right_pad = 1,
-        -- 语言信息
-        position = "right",
-        language_icon = true,
-        language_name = true,
-        -- avoid making headings ugly
-        highlight_inline = "RenderMarkdownCodeInfo",
-      },
-      completions = {
-        blink = { enabled = true },
-        lsp = { enabled = true },
-      },
-      dash = { width = 20 },
-      heading = {
-        position = "inline",
-        sign = false,
-        border = true,
-        border_virtual = true,
-        icons = { "󰼏 ", "󰼐 ", "󰼑 ", "󰼒 ", "󰼓 ", "󰼔 " },
-      },
-      indent = {
-        enabled = true,
-        skip_level = 0,
-        skip_heading = true,
-      },
-      latex = { bottom_pad = 1 },
-      link = {
-        wiki = { icon = " ", highlight = "RenderMarkdownWikiLink", scope_highlight = "RenderMarkdownWikiLink" },
-        image = "󰋵 ",
-        email = " ",
-        hyperlink = "󰌷 ",
-        custom = {
-          python = { pattern = "%.py$", icon = "󰌠 " },
-          github = { pattern = "github", icon = " " },
-          gitlab = { pattern = "gitlab", icon = "󰮠 " },
-          youtube = { pattern = "youtube", icon = " " },
-          cern = { pattern = "cern.ch", icon = " " },
-        },
-      },
-      pipe_table = {
-        alignment_indicator = "─",
-        border = { "╭", "┬", "╮", "├", "┼", "┤", "╰", "┴", "╯", "│", "─" },
-      },
-      sign = { enabled = false },
-      win_options = { concealcursor = { rendered = "nvc" } },
-    },
     ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
-    config = function(_, opts)
-      require("render-markdown").setup(opts)
-      Snacks.toggle({
-        name = "Render Markdown",
-        get = function()
-          return require("render-markdown.state").enabled
-        end,
-        set = function(enabled)
-          local m = require("render-markdown")
-          if enabled then
-            m.enable()
-          else
-            m.disable()
-          end
-        end,
-      }):map("<leader>um")
+    config = function()
+      require("config.render-markdown")
     end,
+  },
+  {
+    "bullets-vim/bullets.vim",
+    ft = { "markdown" },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    ft = { "tex", "markdown", "typst" },
+    opts = {
+      default = {
+        dir_path = "./附件",
+        use_absolute_path = false,
+        copy_images = true,
+        prompt_for_file_name = false,
+        file_name = "%y%m%d-%H%M%S",
+        extension = "avif",
+        process_cmd = "magick convert - -quality 75 avif:-",
+      },
+      filetypes = {
+        markdown = {
+          template = "![image$CURSOR]($FILE_PATH)",
+        },
+        tex = {
+          dir_path = "./figs",
+          extension = "png",
+          process_cmd = "",
+          template = [[
+    \begin{figure}[h]
+      \centering
+      \includegraphics[width=0.8\textwidth]{$FILE_PATH}
+    \end{figure}
+        ]], ---@type string | fun(context: table): string
+        },
+        typst = {
+          dir_path = "./figs",
+          extension = "png",
+          process_cmd = "magick convert - -density 300 png:-",
+          template = [[
+          #align(center)[#image("$FILE_PATH", height: 80%)]
+          ]],
+        },
+      },
+    },
+    keys = {
+      { "<leader>P", "<cmd>PasteImage<cr>", desc = "从系统剪切板粘贴图像文件" },
+    },
   },
 }
