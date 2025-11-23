4fd4c074dd7f2e90d82acc846c0de3a21033e89f
Xu Jian <838178533@qq.com>
2025-11-23 21:47:24 +0800

    theme file

---
+++ .config/fzf/fzf-themes.sh
@@ -0,0 +1,358 @@
+# fzf主题
+# Papercolor light
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+# 	--color=fg:#4d4d4c,bg:#eeeeee,hl:#d7005f
+# 	--color=fg+:#d75f00,bg+:#007175,hl+:#fd3f7f
+# 	--color=info:#4271ae,prompt:#8959a8,pointer:#d7005f
+# 	--color=marker:#4271ae,spinner:#4271ae,header:#4271ae
+# 	--color=preview-fg:#bcbcbc,preview-bg:#005f87
+# 	--color=border:#444444,query:#00aaee'
+
+# Papercolor dark
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+# 	--color=fg:#d0d0d0,bg:#1c1c1c,hl:#afd700
+# 	--color=fg+:#1c1c1c,bg+:#af87d7,hl+:#af005f
+# 	--color=info:#8700af,prompt:#00afaf,pointer:#ffaf00
+# 	--color=marker:#ffaf00,spinner:#4271ae,header:#5f8787
+# 	--color=preview-fg:#585858,preview-bg:#1c1c2c
+# 	--color=border:#4d4f5f,query:#00afaf,gutter:#444444'
+
+# SpaceCamp
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#  --color=fg:#dedede,bg:#121212,hl:#666666
+#  --color=fg+:#eeeeee,bg+:#282828,hl+:#cf73e6
+#  --color=info:#cf73e6,prompt:#FF0000,pointer:#cf73e6
+#  --color=marker:#f0d50c,spinner:#cf73e6,header:#91aadf
+#  --color=preview-bg:#223345
+#  --color=border:#778899,query:#b7c741'
+# --color=query:#2874af
+
+# Dracula
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#     --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9
+#     --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
+#     --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
+#     --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
+#     --color=border:#6272a4'
+
+# web color
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#     --color=fg:#d0d0d0,bg:#121212,hl:#5f87af
+# 	--color=fg+:#d0d0d0,bg+:#262626,hl+:#5fd7ff
+# 	--color=info:#afaf87,prompt:#d7005f,pointer:#af5fff
+# 	--color=marker:#87ff00,spinner:#af5fff,header:#87afaf
+#     --color=query:#d7005f'
+
+#sonokai:
+#sonokai-default:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#     --color=fg:#e1e3e4,bg:#2b2d3a,hl:#7e8294
+# 	--color=fg+:#e1e3e4,bg+:#333648,hl+:#fb617e
+# 	--color=info:#fb617e,prompt:#fb617e,pointer:#fb617e
+# 	--color=marker:#fb617e,spinner:#bb97ee,header:#7e8294
+# 	--color=border:#7e8294,preview-bg:#363a4e'
+
+# sonokai-andromeda:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+# 	--color=fg:#e1e3e4,bg:#2b2d3a,hl:#9ed06c
+# 	--color=fg+:#e1e3e4,bg+:#333648,hl+:#9ed06c
+# 	--color=info:#edc763,prompt:#fb617e,pointer:#6dcae8
+# 	--color=marker:#6dcae8,spinner:#edc763,header:#6dcae8
+#     --color=border:#7e8294'
+#
+# sonokai-atlantis:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#     --color=fg:#e1e3e4,bg:#2a2f38,hl:#9dd274
+# 	--color=fg+:#e1e3e4,bg+:#333846,hl+:#9dd274
+# 	--color=info:#eacb64,prompt:#ff6578,pointer:#72cce8
+# 	--color=marker:#72cce8,spinner:#eacb64,header:#72cce8
+#     --color=border:#828a9a'
+#
+# sonokai-shusia:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#e3e1e4,bg:#2d2a2e,hl:#9ecd6f
+# 	--color=fg+:#e3e1e4,bg+:#37343a,hl+:#9ecd6f
+# 	--color=info:#e5c463,prompt:#f85e84,pointer:#7accd7
+# 	--color=marker:#7accd7,spinner:#e5c463,header:#7accd7
+#   --color=border:#848089'
+#
+# sonokai-maia:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#e1e2e3,bg:#273136,hl:#9cd57b
+# 	--color=fg+:#e1e2e3,bg+:#313b42,hl+:#9cd57b
+# 	--color=info:#e3d367,prompt:#f76c7c,pointer:#78cee9
+# 	--color=marker:#78cee9,spinner:#e3d367,header:#78cee9
+#   --color=border:#82878b'
+#
+# sonokai-espresso:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#e4e3e1,bg:#312c2b,hl:#a6cd77
+# 	--color=fg+:#e4e3e1,bg+:#393230,hl+:#a6cd77
+# 	--color=info:#f0c66f,prompt:#f86882,pointer:#81d0c9
+# 	--color=marker:#81d0c9,spinner:#f0c66f,header:#81d0c9
+#   --color=border:#90817b'
+#
+# everforest-dark:
+# everforest-soft:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#     --color=fg:#d3c6aa,bg:#333c43,hl:#a7c080
+# 	--color=fg+:#d3c6aa,bg+:#3a464c,hl+:#83c092
+# 	--color=info:#83c092,prompt:#e69875,pointer:#7fbbb3
+# 	--color=marker:#dbbc7f,spinner:#dbbc7f,header:#859289
+#     --color=border:#859289'
+#
+# everforest-medium:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#d3c6aa,bg:#2d353b,hl:#a7c080
+# 	--color=fg+:#d3c6aa,bg+:#343f44,hl+:#83c092
+# 	--color=info:#83c092,prompt:#e69875,pointer:#7fbbb3
+# 	--color=marker:#dbbc7f,spinner:#dbbc7f,header:#859289
+#   --color=border:#859289'
+#
+# everforest-hard:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#d3c6aa,bg:#272e33,hl:#a7c080
+# 	--color=fg+:#d3c6aa,bg+:#2e383c,hl+:#83c092
+# 	--color=info:#83c092,prompt:#e69875,pointer:#7fbbb3
+# 	--color=marker:#dbbc7f,spinner:#dbbc7f,header:#859289
+#   --color=border:#859289'
+#
+# everforest-light:
+# everforest-soft:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#5c6a72,bg:#f3ead3,hl:#8da101
+# 	--color=fg+:#5c6a72,bg+:#eae4ca,hl+:#35a77c
+# 	--color=info:#35a77c,prompt:#f57d26,pointer:#3a94c5
+# 	--color=marker:#dfa000,spinner:#dfa000,header:#939f91
+#   --color=border:#939f91'
+
+# everforest-medium:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#5c6a72,bg:#fdf6e3,hl:#8da101
+# 	--color=fg+:#5c6a72,bg+:#f4f0d9,hl+:#35a77c
+# 	--color=info:#35a77c,prompt:#f57d26,pointer:#3a94c5
+# 	--color=marker:#dfa000,spinner:#dfa000,header:#939f91
+#   --color=border:#939f91'
+
+# everforest-hard:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#5c6a72,bg:#fffbef,hl:#8da101
+# 	--color=fg+:#5c6a72,bg+:#f8f5e4,hl+:#35a77c
+# 	--color=info:#35a77c,prompt:#f57d26,pointer:#3a94c5
+# 	--color=marker:#dfa000,spinner:#dfa000,header:#939f91
+#   --color=border:#939f91'
+
+# Catppuccin-frappe:
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=bg+:#414559,bg:#303446,spinner:#F2D5CF,hl:#E78284 \
+# --color=fg:#C6D0F5,header:#E78284,info:#CA9EE6,pointer:#F2D5CF \
+# --color=marker:#BABBF1,fg+:#C6D0F5,prompt:#CA9EE6,hl+:#E78284 \
+# --color=selected-bg:#51576D \
+# --color=border:#737994,label:#C6D0F5"
+
+# Catppuccin-latte:
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
+# --color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
+# --color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
+# --color=selected-bg:#BCC0CC \
+# --color=border:#9CA0B0,label:#4C4F69"
+
+# Catppuccin-macchiato:
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=bg+:#363A4F,bg:#24273A,spinner:#F4DBD6,hl:#ED8796 \
+# --color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+# --color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+# --color=selected-bg:#494D64 \
+# --color=border:#6E738D,label:#CAD3F5"
+
+# Catppuccin-mocha:
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+# --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+# --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+# --color=selected-bg:#45475A \
+# --color=border:#6C7086,label:#CDD6F4"
+
+# rose-pine-dawn:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#   --color=fg:#797593,bg:#faf4ed,hl:#d7827e
+# 	--color=fg+:#575279,bg+:#f2e9e1,hl+:#d7827e
+# 	--color=border:#dfdad9,header:#286983,gutter:#faf4ed
+# 	--color=spinner:#ea9d34,info:#56949f
+# 	--color=pointer:#907aa9,marker:#b4637a,prompt:#797593'
+#
+# rose-pine-moon:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+# 	--color=fg:#908caa,bg:#232136,hl:#ea9a97
+# 	--color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
+# 	--color=border:#44415a,header:#3e8fb0,gutter:#232136
+# 	--color=spinner:#f6c177,info:#9ccfd8
+# 	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa'
+#
+# rose-pine:
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#     --color=fg:#908caa,bg:#191724,hl:#ebbcba
+# 	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+# 	--color=border:#403d52,header:#31748f,gutter:#191724
+# 	--color=spinner:#f6c177,info:#9ccfd8
+# 	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa'
+
+# Ayu Mirage
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+# 	--color=fg:#cbccc6,bg:#1f2430,hl:#707a8c
+# 	--color=fg+:#707a8c,bg+:#191e2a,hl+:#ffcc66
+# 	--color=info:#73d0ff,prompt:#707a8c,pointer:#cbccc6
+# 	--color=marker:#73d0ff,spinner:#73d0ff,header:#d4bfff'
+#
+# tokyonight_day
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#3760bf,bg:#e1e2e7,hl:#b15c00 \
+# --color=fg+:#3760bf,bg+:#c4c8da,hl+:#b15c00 \
+# --color=info:#2e7de9,prompt:#007197,pointer:#007197 \
+# --color=marker:#587539,spinner:#587539,header:#587539 \
+# --color=border:#3B3B58"
+#
+# tokyonight_moon
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+--color=fg:#c8d3f5,bg:#222436,hl:#ff966c \
+--color=fg+:#c8d3f5,bg+:#2f334d,hl+:#ff966c \
+--color=info:#82aaff,prompt:#86e1fc,pointer:#86e1fc \
+--color=marker:#c3e88d,spinner:#c3e88d,header:#c3e88d \
+--color=border:#3B3B58"
+#
+# tokyonight_night
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#c0caf5,bg:#1a1b26,hl:#ff9e64 \
+# --color=fg+:#c0caf5,bg+:#292e42,hl+:#ff9e64 \
+# --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
+# --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a \
+# --color=border:#3B3B58"
+#
+# tokyonight_storm
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#c0caf5,bg:#24283b,hl:#ff9e64 \
+# --color=fg+:#c0caf5,bg+:#292e42,hl+:#ff9e64 \
+# --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
+# --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a \
+# --color=border:#3B3B58"
+
+# Seoul256
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#D9D9D9,bg:#4B4B4B,hl:#719872 \
+# --color=fg+:#D9D9D9,bg+:#3F3F3F,hl+:#98BC99 \
+# --color=info:#BDBB72,prompt:#98BEDE,pointer:#E12672 \
+# --color=marker:#E17899,,spinner:#98BC99,header:#719872 \
+# --color=,border:#6B6B6B,query:#98BC99"
+
+# nightfox
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#575279,bg:#faf4ed,hl:#5ca7b4 \
+# --color=fg+:#4c4769,bg+:#ebdfe4,hl+:#50848c \
+# --color=info:#a8a3b3,prompt:#56949f,pointer:#56949f \
+# --color=marker:#618774,spinner:#a8a3b3,header:#625c87"
+
+# neofusion
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=bg+:#031B26,bg:#06101e,spinner:#fd5e3a,hl:#e2d9c5 \
+# --color=fg:#08435E,header:#e2d9c5,info:#35b5ff,pointer:#fa7a61 \
+# --color=marker:#fd5e3a,fg+:#66def9,prompt:#35b5ff,hl+:#fd5e3a"
+
+# halcyon
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+#  --color=fg:#d7dce2,bg:#1d2433,hl:#c3a6ff \
+#  --color=fg+:#d7dce2,bg+:#2f3b54,hl+:#c3a6ff \
+#  --color=info:#5ccfe6,prompt:#bae67e,pointer:#ffae57 \
+#  --color=marker:#ef6b73,spinner:#ffd580,header:#8695b7"
+
+# night rain
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#f8f8f2,bg:#32324b,hl:#8be9fd \
+# --color=fg+:#f8f8f2,bg+:#616175,hl+:#f1fa8c \
+# --color=info:#8be9fd,prompt:#a8ffde,pointer:#f1fa8c \
+# --color=marker:#8be9fd,spinner:#8be9fd,header:#ff5555"
+
+# day rain
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+#     --color=fg:#1f2328,bg:#fafbfc,hl:#0366d6 \
+#     --color=fg+:#1f2328,bg+:#e1e4e8,hl+:#ff841f \
+#     --color=info:#0366d6,prompt:#28A745,pointer:#ff841f \
+#     --color=marker:#0366d6,spinner:#0366d6,header:#d73a49"
+
+# morhetz/gruvbox
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#ebdbb2,bg:#32302f,hl:#928374 \
+# --color=fg+:#ebdbb2,bg+:#3c3836,hl+:#fb4934 \
+# --color=info:#8ec07c,prompt:#fb4934,pointer:#fb4934 \
+# --color=marker:#fb4934,spinner:#fb4934,header:#928374 \
+# --color=border:#7c6f64"
+
+# gruvbox dark
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#ebdbb2,bg:#282828,hl:#d65d0e \
+# --color=fg+:#fbf1c7,bg+:#3c3836,hl+:#b8bb26 \
+# --color=info:#83a598,prompt:#8ec07c,pointer:#d79921 \
+# --color=marker:#fe8019,spinner:#b16286,header:#fb4934 \
+# --color=border:#7c6f64"
+
+# TokyoNight
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#B0B3CC,bg:#1A1B26,hl:#BB9AF7 \
+# --color=fg+:#1F1F28,bg+:#2A2A3D,hl+:#F7768E \
+# --color=info:#7AA2F7,prompt:#C0CAF5,pointer:#F7768E \
+# --color=marker:#E0AF68,spinner:#9ECE6A,header:#FF6C6B \
+# --color=border:#3B3B58"
+
+# SolarizedDark
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#839496,bg:#002b36,hl:#859900 \
+# --color=fg+:#93a1a1,bg+:#073642,hl+:#b58900 \
+# --color=info:#268bd2,prompt:#2aa198,pointer:#b58900 \
+# --color=marker:#cb4b16,spinner:#d33682,header:#dc322f
+# --color=border:#586e75"
+
+# SublimeSnazzy
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#F8F8F2,bg:#272822,hl:#E6DB74 \
+# --color=fg+:#F8F8F2,bg+:#44475A,hl+:#F92672 \
+# --color=info:#66D9EF,prompt:#F92672,pointer:#F8F8F2 \
+# --color=marker:#FD971F,spinner:#A6E22E,header:#F92672 \
+# --color=border:#C5C8C6"
+
+# MonokaiExtendedDark
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#F8F8F2,bg:#272822,hl:#E6DB74 \
+# --color=fg+:#E6DB74,bg+:#272822,hl+:#F92672 \
+# --color=info:#66D9EF,prompt:#F92672,pointer:#F8F8F2 \
+# --color=marker:#FD971F,spinner:#A6E22E,header:#F92672 \
+# --color=border:#C5C8C6,query:#A6E22E"
+
+# dracula
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#f8f8f2,bg:#282a36,hl:#ff79c6 \
+# --color=fg+:#f8f8f2,bg+:#44475a,hl+:#ffb86c \
+# --color=info:#8be9fd,prompt:#ff5555,pointer:#50fa7b \
+# --color=marker:#ffb86c,spinner:#bd93f9,header:#ff79c6 \
+# --color=border:#6272a4,query:#bd93f9"
+
+# kanagawa-paper-canvas
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#758380,bg:#e1e1de,hl:#9e7e98,
+# --color=fg+:#b8805e,bg+:#d8d8d2,hl+:#c27672
+# --color=header:#b28d77,info:#809ba7,pointer:#516e7d
+# --color=marker:#516e7d,prompt:#c27672,spinner:#7e8faf"
+
+# kanagawa-paper-ink
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=bg:#1f1f28,fg:#a292a3,hl:#938AA9 \
+# --color=fg+:#DCD7BA,bg+:#2A2A37,hl+:#c4746e \
+# --color=header:#b6927b,info:#658594,pointer:#7AA89F \
+# --color=marker:#7AA89F,prompt:#c4746e,spinner:#8ea49e"
+
+# tomasr/molokai
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+# --color=fg:#F8F8F2,bg:#1B1D1E,hl:#7E8E91 \
+# --color=fg+:#F8F8F2,bg+:#293739,hl+:#F92672 \
+# --color=info:#A6E22E,prompt:#F92672,pointer:#A6E22E \
+# --color=marker:#F92672,spinner:#E6DB74,header:#7E8E91 \
+# --color=border:#808080"
