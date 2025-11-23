6d4a0f10910fb0eb9ba22b7fdaaac40335af15d8
Xu Jian <838178533@qq.com>
2025-11-23 23:06:22 +0800

    update from office website

--- .config/hypr/scripts/swww_randomize.sh
+++ .config/hypr/scripts/swww_randomize.sh
@@ -1,33 +1,26 @@
 #!/usr/bin/env bash
 
-# This script will randomly go through the files of a directory, setting it
-# up as the wallpaper at regular intervals
-#
-# NOTE: this script is in bash (not posix shell), because the RANDOM variable
-# we use is not defined in posix
+DEFAULT_INTERVAL=300 # In seconds
 
-if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
-	echo "用法:
-	$0 <背景图片目录路径>"
-	exit 1
+if [ $# -lt 1 ] || [ ! -d "$1" ]; then
+    printf "用法:\n\t\e[1m%s\e[0m \e[4m目录\e[0m [\e[4m间隔\e[0m]\n" "$0"
+    printf "\t随机更换背景图\n"
+    exit 1
 fi
 
-# Edit below to control the images transition
-export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_STEP=2
-export SWWW_TRANSITION=any
-
-# This controls (in seconds) when to switch to the next image
-INTERVAL=300
+# See swww-img(1)
+RESIZE_TYPE="fit"
+export SWWW_TRANSITION_FPS="${SWWW_TRANSITION_FPS:-60}"
+export SWWW_TRANSITION_STEP="${SWWW_TRANSITION_STEP:-2}"
 
 while true; do
-	find "$1" |
-		while read -r img; do
-			echo "$((RANDOM % 1000)):$img"
-		done |
-		sort -n | cut -d':' -f2- |
-		while read -r img; do
-			swww img "$img"
-			sleep $INTERVAL
-		done
+    find "$1" -type f |
+        while read -r img; do
+            echo "$(</dev/urandom tr -dc a-zA-Z0-9 | head -c 8):$img"
+        done |
+        sort -n | cut -d':' -f2- |
+        while read -r img; do
+            swww img --resize="$RESIZE_TYPE" "$img"
+            sleep "${2:-$DEFAULT_INTERVAL}"
+        done
 done
