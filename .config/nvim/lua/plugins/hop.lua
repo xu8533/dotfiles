95be3a13307cdf5eabf593650a891337b011723b
Xu Jian <838178533@qq.com>
2025-12-24 22:05:20 +0800

    add new keybind

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -1,5 +1,6 @@
 return {
   "smoka7/hop.nvim",
+  version = "*",
   opts = {
     case_insensitive = false,
     create_hl_autocmd = false, -- 使用hop自带高亮色
@@ -15,10 +16,11 @@ return {
     { "hw", "<cmd>HopWord<CR>", desc = "Hop word motion" },
     { "hn", "<cmd>HopNodes<CR>", desc = "Hop treesitter nodes motion" },
     { "hp", "<cmd>HopPattern<CR>", desc = "Hop pattern motion" },
-    { "hP", "<cmd>HopPasteChar1<CR>", desc = "Hop paste without motion" },
-    { "hpl", "<cmd>HopPasteChar1CurrentLine<CR>", desc = "Hop paste currentline without motion" },
-    -- { "h1c", "<cmd>HopChar1<CR>", desc = "Hop one char motion" },
+    -- { "hP", "<cmd>HopChar1<CR>", desc = "Hop one char motion" },
     -- { "h2c", "<cmd>HopChar2<CR>", desc = "Hop two char motion" },
+    { "hca", "<cmd>HopAnywhereCurrentLine<CR>", desc = "Hop any where currentlinec motion" },
     { "hcl", "<cmd>HopWordCurrentLine<CR>", desc = "Hop current line motion" },
+    { "hpl", "<cmd>HopChar1CurrentLine<CR>", desc = "hop当前行匹配模式" },
+    { "hpm", "<cmd>HopPatternMW<CR>", desc = "Hop多窗口匹配模式" },
   },
 }
