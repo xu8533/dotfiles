ea459b888b7caf9e994c0f4eee5c4ca6f39bf3ba
Xu Jian <838178533@qq.com>
2026-07-13 13:47:09 +0800

    dash instead of sh in fileManager

--- .config/hypr/variables.lua
+++ .config/hypr/variables.lua
@@ -1,7 +1,8 @@
 -- 应用设定
 terminal = "kitty"
 codeEditor = "nvim"
-fileManager = "kitty --class=LF -1 sh -c lf"
+-- fileManager = "kitty --class=LF -1 dash -c lf"
+fileManager = "foot --app-id=LF --title=FileMgr_LF dash -c lf"
 browser = "qutebrowser"
 taskManager = "command -v btop && kitty --class=taskmanager -1 dash -c btop"
 
